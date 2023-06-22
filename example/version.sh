4fd7b1362987cb0b1305bbd423c2ff559406eedd
Saejin Mahlau-Heinert <saejinmh@va.wagner.com>
2023-06-22 12:22:42 -0500

    Update example to allow variable passing

--- example/version.sh
+++ example/version.sh
@@ -3,7 +3,7 @@
 USE_JAVA=true
 
 if [ "$USE_JAVA" = "true" ] && [ -f Version.class ]; then
-    java -classpath . Version
+    java -classpath . Version "$@"
 else
     VERSION="2.0.0"
     if [ "$1" = "--include-build" ]; then
